03512a4cc3df2bc7af642e94e172a5049d195bc9
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-19 16:09:42 +0300

    19.05.22

---
+++ main.js
@@ -0,0 +1,45 @@
+// var names= prompt();
+// if(names[0]=="a"){
+//     console.log("a");
+// }
+// else{
+//     console.log("bye");
+// }
+
+// var famely=prompt();
+// if(famely[famely.length-1]=="e"){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// var mystr="yehuda";
+// console.log(mystr.toUpperCase());
+
+// var number="12,3,455";
+// console.log(number.match());
+
+// var number=+prompt();
+// var i=0;
+// var num=1;
+// while(i<number){
+//     num*=number
+//     number--
+//     console.log(num);
+// }
+
+// var num=prompt();
+// var i=5;
+// var number=num*1;
+
+// while(i>0){
+//     if(number>num){
+//         console.log(number);
+//     else{
+//             console.log(num);
+//         }
+//     }
+//     i--
+// }
+
