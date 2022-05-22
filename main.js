f7e1161fc42bc94bc6c6108578f25c9f14b7eb76
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-23 01:23:22 +0300

    22.05.22

--- main.js
+++ main.js
@@ -43,3 +43,151 @@
 //     i--
 // }
 
+// function getString(){
+//     var stringnum=[];
+//     for(var i=0; i<stringnum.length;i++){
+//         console.log(stringnum[i]);
+//     } 
+// }
+
+// function stringBackString(){
+//     var str=["hello"];
+//     var newstr[""];
+//     for(var i=0;)
+// }
+
+// function stringBackString(){
+//     var str=[""];
+//     for(var i=0;i<str.length;i++){
+//         if(i%2==0){
+//             str[i].
+//         }
+//     }
+// }
+
+// function getName(){
+//     var str =[""];
+//     if(str[0]=="A"){
+//         console.log("hello");
+//     }
+//     else{
+//         console.log("bye");
+//     }
+// }
+
+// function getFamely(){
+//     var str=prompt();
+//     if(str[str.length-1]=="E"){
+//         console.log("yes");
+//     }
+//     else{
+//         console.log("no");
+//     }
+// }
+// getFamely()
+
+// function stringMorNumber(){
+//     var str="";
+//     if(str.length>5){
+//         console.log("long");
+//     }
+//     else{
+//         console.log("short");
+//     }
+// }
+// stringMorNumber()
+
+// function foundStringA(){
+//     var str="";
+//     for(var i=0; i<str.length;i++){
+//         if(str[i]==a){
+//             console.log(i);
+//         }
+//         else{
+//             console.log("not found")
+//         }
+//     }
+// }
+// foundStringA()
+
+// function foundStringR(){
+//     var str="";
+//     for(var i=0; i<str.length; i++){
+//         if(str[i]=="r"){
+//             console.log(true);
+//         }
+//         else{
+//             console.log(false);
+//         }
+//     }
+// }
+// foundStringR()
+
+// function stringFound(){
+//     var str="";
+//     var tav="t";
+//     for(var i=0;i<str.length;i++){
+//         if(str[i]==tav){
+//             console.log(true);
+//         }
+//         else{
+//             console.log(false);
+//         }
+//     }
+// }
+// stringFound()
+
+
+//לבדוק מחר 22.05.22
+// function foundLlastIndex(){
+//     var str="";
+//     var tav="t";
+//     for(var i=0; i<str.length;i--){
+//         str.lastIndexOf();
+//         if(str[i]==tav){
+//             console.log(i);
+//         }
+//     }
+//     else{
+//         console.log("not tav in the string");
+//     }
+// }
+
+//לבדוק עם יעקב למה לא עבר sub
+// function backTavim(){
+//     var str="sayonara";
+//     var newstr =str.substring(0,3);
+//     console.log(newstr);
+// }
+// backTavim()
+
+// function back4Tavim(){
+//     var str="sayonara";
+//     var newstr=str.substring(2,4);
+//     console.log(newstr);
+// }
+// back4Tavim()
+
+// function backAllo(){
+//     var str="sayonara";
+//     var newstr=str.substring(3,);
+//     console.log(newstr);
+// }
+// backAllo()
+
+// function getStringAndIndex(){
+//     var str="";
+//     var num=[];
+//     if(str.length<num[i]){
+//         console.log("index to big");
+//     }
+//     else{
+//         console.log(str.substring(num,));
+//     }
+// }
+// getStringAndIndex()
+
+
+
+
+
